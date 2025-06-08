73780ac600615315bea95a4edfdeba7777c8cf67
swapnil-kadam <swapnil9960976776@gmil.com>
2025-06-08 11:25:31 +0530

    Controllers file are complate code write

---
+++ backend/index.js
@@ -0,0 +1,67 @@
+
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
+
+const { initRepo } = require("./controllers/init");
+const { addRepo } = require("./controllers/add");
+const { commitRepo } = require("./controllers/commit");
+const { pushRepo } = require("./controllers/push");
+const { pullRepo } = require("./controllers/pull");
+const { revertRepo } = require("./controllers/revert");
+
+dotenv.config();
+
+yargs(hideBin(process.argv))
+  .command("start", "Starts a new server", {}, startServer)
+  .command("init", "Initialise a new repository", {}, initRepo)
+  .command(
+    "add <file>",
+    "Add a file to the repository",
+    (yargs) => {
+      yargs.positional("file", {
+        describe: "File to add to the staging area",
+        type: "string",
+      });
+    },
+    (argv) => {
+      addRepo(argv.file);
+    }
+  )
+  .command(
+    "commit <message>",
+    "Commit the staged files",
+    (yargs) => {
+      yargs.positional("message", {
+        describe: "Commit message",
+        type: "string",
+      });
+    },
+    (argv) => {
+      commitRepo(argv.message);
+    }
+  )
+  .command("push", "Push commits to S3", {}, pushRepo)
+  .command("pull", "Pull commits from S3", {}, pullRepo)
+  .command(
+    "revert <commitID>",
+    "Revert to a specific commit",
+    (yargs) => {
+      yargs.positional("commitID", {
+        describe: "Comit ID to revert to",
+        type: "string",
+      });
+    },
+    (argv) => {
+      revertRepo(argv.commitID);
+    }
+  )
+  .demandCommand(1, "You need at least one command")
+  .help().argv;
+
+function startServer() {
+  const app = express();
+  const port = process.env.PORT || 3000;
+  httpServer.listen(port, () => {
+    console.log(`Server is running on PORT ${port}`);
+  });
+}
